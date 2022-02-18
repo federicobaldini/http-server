f5484f9d14fa5f21b4ee307275641cb2623164f2
federicobaldini <federico-baldini@hotmail.com>
2022-02-18 12:58:29 +0100

    defined Server structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+  let server = Server::new("127.0.0.1:5000".to_string());
+  server.run();
+}
+
+struct Server {
+  address: String,
+}
+
+impl Server {
+  fn new(address: String) -> Self {
+    Self { address: address }
+  }
+
+  fn run(self) {
+    println!("Listening on {}", self.address);
+  }
 }
