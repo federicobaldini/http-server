1a151aebba752ea6ebc007685c940ce0869c0581
federicobaldini <federico-baldini@hotmail.com>
2022-02-20 12:05:03 +0100

    checking for new connections

--- src/server.rs
+++ src/server.rs
@@ -12,5 +12,16 @@ impl Server {
     println!("Listening on {}", self.address);
 
     let listener = TcpListener::bind(&self.address).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((stream, _)) => {
+          /* manage the new connection */
+        },
+        Err(error) => {
+          println!("Failed to establish a connection: {}", error);
+        }
+      }
+    }
   }
 }
