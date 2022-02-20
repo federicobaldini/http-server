9323d5055dba5f4bfad6dab23d30e1e9fe721561
federicobaldini <federico-baldini@hotmail.com>
2022-02-20 11:46:39 +0100

    added listener for TCP connections

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,16 @@
+use std::net::TcpListener;
+
 pub struct Server {
   address: String,
 }
+
 impl Server {
   pub fn new(address: String) -> Self {
     Self { address: address }
   }
   pub fn run(self) {
     println!("Listening on {}", self.address);
+
+    let listener = TcpListener::bind(&self.address).unwrap();
   }
 }
