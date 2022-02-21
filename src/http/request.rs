d40d9bac079cdfe4a634b9c8c068599c87137e58
federicobaldini <federico-baldini@hotmail.com>
2022-02-21 20:37:18 +0100

    added parser from buffer to Request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
   path: String,
@@ -6,4 +7,12 @@ pub struct Request {
   method: Method,
   headers: String,
   body: String,
+}
+
+impl TryFrom<&[u8]> for Request {
+  type Error = String;
+
+  fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    unimplemented!();
+  }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
-use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
+use std::net::TcpListener;
 
 pub struct Server {
   address: String,
@@ -21,12 +24,18 @@ impl Server {
           match stream.read(&mut buffer) {
             Ok(_) => {
               println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-            },
+              match Request::try_from(&buffer[..]) {
+                Ok(request) => {}
+                Err(error) => {
+                  println!("Failed to parse a request: {}", error);
+                }
+              }
+            }
             Err(error) => {
               println!("Failed to read from connection: {}", error);
             }
           }
-        },
+        }
         Err(error) => {
           println!("Failed to establish a connection: {}", error);
         }
