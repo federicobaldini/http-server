5fc61da5b205c30f75b6aea8379a8b983664cd70
federicobaldini <federico-baldini@hotmail.com>
2022-02-22 11:39:09 +0100

    improved Request parser

--- src/http/request.rs
+++ src/http/request.rs
@@ -17,9 +17,27 @@ impl TryFrom<&[u8]> for Request {
   type Error = ParseError;
 
   fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-    let result = str::from_utf8(buf)?;
+    let request = str::from_utf8(buf)?;
+
+    let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+    let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+    if protocol != "HTTP/1.1" {
+      return Err(ParseError::InvalidProtocol);
+    }
+
     unimplemented!();
   }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+  for (i, c) in request.chars().enumerate() {
+    if c == ' ' || c == '\r' || c == '\n' {
+      return Some((&request[..i], &request[i + 1..]));
+    }
+  }
+  None
 }
 
 pub enum ParseError {
