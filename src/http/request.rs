7c066cbc6944b4913d29bf03742dfa977490b03f
federicobaldini <federico-baldini@hotmail.com>
2022-02-23 21:14:23 +0100

    converted buffer data to Request struct

--- src/http/request.rs
+++ src/http/request.rs
@@ -5,18 +5,20 @@ use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str;
 use std::str::Utf8Error;
 
-pub struct Request {
-  path: String,
-  query_string: Option<String>,
+pub struct Request<'buf> {
+  path: &'buf str,
+  query_string: Option<&'buf str>,
   method: Method,
+  /*
   headers: String,
   body: String,
+  */
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf>  TryFrom<&'buf [u8]> for Request<'buf> {
   type Error = ParseError;
 
-  fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+  fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
     let request = str::from_utf8(buf)?;
 
     let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
@@ -35,7 +37,11 @@ impl TryFrom<&[u8]> for Request {
       path = &path[..i];
     }
 
-    unimplemented!();
+    Ok(Self {
+      path,
+      query_string,
+      method
+    })
   }
 }
 
