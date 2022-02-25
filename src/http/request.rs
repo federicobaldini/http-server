bb147dd3c4361ef621fbeb8dafcefb17b62a2dc3
federicobaldini <federico-baldini@hotmail.com>
2022-02-25 11:29:20 +0100

    implemented QueryString module

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::{Method, MethodError};
+use super::QueryString;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -7,7 +8,7 @@ use std::str::Utf8Error;
 
 pub struct Request<'buf> {
   path: &'buf str,
-  query_string: Option<&'buf str>,
+  query_string: Option<QueryString<'buf>>,
   method: Method,
   /*
   headers: String,
@@ -15,7 +16,7 @@ pub struct Request<'buf> {
   */
 }
 
-impl<'buf>  TryFrom<&'buf [u8]> for Request<'buf> {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
   type Error = ParseError;
 
   fn try_from(buf: &'buf [u8]) -> Result<Self, Self::Error> {
@@ -33,14 +34,14 @@ impl<'buf>  TryFrom<&'buf [u8]> for Request<'buf> {
 
     let mut query_string = None;
     if let Some(i) = path.find('?') {
-      query_string = Some(&path[i + 1..]);
+      query_string = Some(QueryString::from(&path[i + 1..]));
       path = &path[..i];
     }
 
     Ok(Self {
       path,
       query_string,
-      method
+      method,
     })
   }
 }
