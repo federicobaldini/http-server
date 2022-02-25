fa11ac9737fd3e9241d8aaa0c58e98b026b586ff
federicobaldini <federico-baldini@hotmail.com>
2022-02-25 11:40:07 +0100

    added derive debug attribute to test code

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,6 +6,7 @@ use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::str;
 use std::str::Utf8Error;
 
+#[derive(Debug)]
 pub struct Request<'buf> {
   path: &'buf str,
   query_string: Option<QueryString<'buf>>,
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,5 @@
 use crate::http::Request;
 use std::convert::TryFrom;
-use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -25,7 +24,9 @@ impl Server {
             Ok(_) => {
               println!("Received a request: {}", String::from_utf8_lossy(&buffer));
               match Request::try_from(&buffer[..]) {
-                Ok(request) => {}
+                Ok(request) => {
+                  dbg!(request);
+                }
                 Err(error) => {
                   println!("Failed to parse a request: {}", error);
                 }
