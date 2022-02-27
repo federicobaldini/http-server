77354d4f04cddbb3a29dcbfdc2c69dc5c77eac71
federicobaldini <federico-baldini@hotmail.com>
2022-02-27 11:46:35 +0100

    added new Response module

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,7 +2,9 @@ pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
+pub use response::Response;
 
 pub mod method;
 pub mod query_string;
 pub mod request;
+pub mod response;
