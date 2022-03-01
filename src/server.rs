532dde457a7914d45c57f0511d3af46ea8c25a16
federicobaldini <federico-baldini@hotmail.com>
2022-03-01 11:37:01 +0100

    added new custom trait Handler

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,16 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
+
+pub trait Handler {
+  fn handle_request(&mut self, request: &Request) -> Response;
+
+  fn handle_bad_request(&mut self, error: &ParseError) -> Response {
+    println!("Failed to parse a request: {}", error);
+    Response::new(StatusCode::BadRequest, None)
+  }
+}
 
 pub struct Server {
   address: String,
@@ -11,7 +20,7 @@ impl Server {
   pub fn new(address: String) -> Self {
     Self { address: address }
   }
-  pub fn run(self) {
+  pub fn run(self, mut handler: impl Handler) {
     println!("Listening on {}", self.address);
 
     let listener = TcpListener::bind(&self.address).unwrap();
@@ -24,17 +33,8 @@ impl Server {
             Ok(_) => {
               println!("Received a request: {}", String::from_utf8_lossy(&buffer));
               let response = match Request::try_from(&buffer[..]) {
-                Ok(request) => {
-                  dbg!(request);
-                  Response::new(
-                    StatusCode::Ok,
-                    Some("<h1>COCONUT SERVER WORKS!</h1".to_string()),
-                  )
-                }
-                Err(error) => {
-                  println!("Failed to parse a request: {}", error);
-                  Response::new(StatusCode::BadRequest, None)
-                }
+                Ok(request) => handler.handle_request(&request),
+                Err(error) => handler.handle_bad_request(&error),
               };
               if let Err(e) = response.send(&mut stream) {
                 println!("Failed to send a response: {}", e);
