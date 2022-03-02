3f62e50f61ec915f6322b53b5f2eba87d618ab15
federicobaldini <federico-baldini@hotmail.com>
2022-03-02 18:26:22 +0100

    routing incoming requests

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,13 +1,17 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
-    Response::new(
-      StatusCode::Ok,
-      Some("<h1>COCONUT SERVER WORKS!</h1".to_string()),
-    )
+    match request.method() {
+      Method::GET => match request.path() {
+        "/" => Response::new(StatusCode::Ok, Some("<h1>Path '/' works!</h1>".to_string())),
+        "/test" => Response::new(StatusCode::Ok, Some("<h1>Path '/test' works!</h1>".to_string())),
+        _ => Response::new(StatusCode::NotFound, None),
+      },
+      _ => Response::new(StatusCode::NotFound, None),
+    }
   }
 }
