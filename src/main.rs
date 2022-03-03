786907667f9f9ca69fd59e87adaef2bad94f0a77
federicobaldini <federico-baldini@hotmail.com>
2022-03-03 10:44:15 +0100

    serving html files

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ mod server;
 mod website_handler;
 
 fn main() {
-  let default_path = format!("{}/punlic", env!("CARGO_MANIFEST_DIR"));
+  let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
   let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+  println!("Public path: {}", public_path);
   let server = Server::new("127.0.0.1:5000".to_string());
   server.run(WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
 
 pub struct WebsiteHandler {
   public_path: String,
@@ -9,13 +10,18 @@ impl WebsiteHandler {
   pub fn new(public_path: String) -> Self {
     Self { public_path }
   }
+
+  fn read_html_file(&self, file_path: &str) -> Option<String> {
+    let path = format!("{}/{}", self.public_path, file_path);
+    fs::read_to_string(path).ok()
+  }
 }
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
     match request.method() {
       Method::GET => match request.path() {
-        "/" => Response::new(StatusCode::Ok, Some("<h1>Path '/' works!</h1>".to_string())),
+        "/" => Response::new(StatusCode::Ok, self.read_html_file("index.html")),
         "/test" => Response::new(
           StatusCode::Ok,
           Some("<h1>Path '/test' works!</h1>".to_string()),
