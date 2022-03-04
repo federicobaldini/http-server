2fffa26c811a310b5a31c6573ca3ac217ce50186
federicobaldini <federico-baldini@hotmail.com>
2022-03-04 13:17:27 +0100

    added check for serving arbitrary files securely

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -11,9 +11,20 @@ impl WebsiteHandler {
     Self { public_path }
   }
 
-  fn read_html_file(&self, file_path: &str) -> Option<String> {
+  fn read_file(&self, file_path: &str) -> Option<String> {
     let path = format!("{}/{}", self.public_path, file_path);
-    fs::read_to_string(path).ok()
+
+    match fs::canonicalize(path) {
+      Ok(path) => {
+        if path.starts_with(&self.public_path) {
+          fs::read_to_string(path).ok()
+        } else {
+          println!("Directory Traversal Attack Attempted: {}", file_path);
+          None
+        }
+      }
+      Err(_) => None,
+    }
   }
 }
 
@@ -21,12 +32,15 @@ impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
     match request.method() {
       Method::GET => match request.path() {
-        "/" => Response::new(StatusCode::Ok, self.read_html_file("index.html")),
+        "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
         "/test" => Response::new(
           StatusCode::Ok,
           Some("<h1>Path '/test' works!</h1>".to_string()),
         ),
-        _ => Response::new(StatusCode::NotFound, None),
+        path => match self.read_file(path) {
+          Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+          None => Response::new(StatusCode::NotFound, None),
+        },
       },
       _ => Response::new(StatusCode::NotFound, None),
     }
