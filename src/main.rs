8892fcd3b11982fcdd7b74b9d08fdeb5b7ceb021
Federico Baldini <federico.baldini7@gmail.com>
2023-01-23 09:12:56 +0100

    Update main.rs
    Added comments.

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ mod server;
 mod website_handler;
 
 fn main() {
+  // The default path for the public files is set to the "public" directory within the current cargo manifest directory
   let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+  // The actual public path is determined by checking for the presence of an environment variable named "PUBLIC_PATH"
+  // If it exists, it is used as the public path. If it does not exist, the default path defined above is used instead.
   let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
   println!("Public path: {}", public_path);
+  // A new server instance is created and bound to the IP address "127.0.0.1" and port 5000
   let server = Server::new("127.0.0.1:5000".to_string());
   server.run(WebsiteHandler::new(public_path));
 }
