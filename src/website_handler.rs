a609eb2e1648ea620be4b3a3b015a2230536b8ae
Federico Baldini <federico.baldini7@gmail.com>
2023-01-23 09:13:20 +0100

    Update website_handler.rs
    Added comments.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -11,11 +11,16 @@ impl WebsiteHandler {
     Self { public_path }
   }
 
+  // The read_file method reads a file from the specified path and returns its contents
   fn read_file(&self, file_path: &str) -> Option<String> {
-    let path = format!("{}/{}", self.public_path, file_path);
+    // Creates a path by combining the public_path field of the struct with the file_path parameter
+    let path: String = format!("{}/{}", self.public_path, file_path);
 
+    // Attempts to get the canonical path of the file
     match fs::canonicalize(path) {
+      // If successful, check if the path starts with the public_path field of the struct
       Ok(path) => {
+        // If the path is safe, read the contents of the file and return it
         if path.starts_with(&self.public_path) {
           fs::read_to_string(path).ok()
         } else {
@@ -28,7 +33,9 @@ impl WebsiteHandler {
   }
 }
 
+// Only the GET method is handled
 impl Handler for WebsiteHandler {
+  // Handles incoming request
   fn handle_request(&mut self, request: &Request) -> Response {
     match request.method() {
       Method::GET => match request.path() {
@@ -37,6 +44,7 @@ impl Handler for WebsiteHandler {
           StatusCode::Ok,
           Some("<h1>Path '/test' works!</h1>".to_string()),
         ),
+        // For all other request paths, try to read the file at the requested path
         path => match self.read_file(path) {
           Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
           None => Response::new(StatusCode::NotFound, None),
