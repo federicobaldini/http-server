1f4ea1633f53f6668eea4a1f24e487d6012725e1
Federico Baldini <federico.baldini7@gmail.com>
2023-01-25 19:36:48 +0100

    Update response.rs
    Added comments.

--- src/http/response.rs
+++ src/http/response.rs
@@ -9,15 +9,18 @@ pub struct Response {
 
 impl Response {
   pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
-    Response { status_code, body }
+    Self { status_code, body }
   }
 
+  // The send method sends the Response to the provided stream
   pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
-    let body = match &self.body {
+    // Converts the body field (string slice) to a String reference
+    let body: &str = match &self.body {
       Some(b) => b,
       None => "",
     };
 
+    // Writes the response to the stream in the format "HTTP/1.1 [status_code] [reason_phrase]\r\n\r\n[body]"
     write!(
       stream,
       "HTTP/1.1 {} {}\r\n\r\n{}",
